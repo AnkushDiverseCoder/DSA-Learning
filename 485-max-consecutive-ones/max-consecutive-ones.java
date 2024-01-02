c1cb1a5769be0705208eb561f51c53ce1b672058
Thakur Ankush Singh <104247253+AnkushDiverseCoder@users.noreply.github.com>
2024-01-02 22:34:18 +0530

    Time: 2 ms (85.20%) | Memory: 45.4 MB (21.01%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -1,24 +1,48 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        int count =0;
-        for(int i=0;i<nums.length;i++){
-            int currEle = nums[i];
-            if(currEle != 1){
-                nums[i]= count;
-                count = 0;
-            }else{
-                count= count+1;
+      // Initialize variables to track the maximum count and current count of consecutive 1s
+        int maxCount=0;
+        int curCount=0;
+          for(int element: nums)
+        {
+            if(element==0)
+            {
+        //reset curCount and maxCount whenever 0 is encountered.
+                if(maxCount<curCount)
+                {
+                    maxCount=curCount;
+                }
+                curCount=0;                
             }
-            if(i == nums.length-1 ){
-                nums[i]=count;
+            else
+            {
+                curCount++;
             }
         }
-        int max = 0;
-        for(int i =0; i<nums.length;i++){
-            if(nums[i]>max){
-                max = nums[i];
-            }
-        }
-        return max;
+        // After the loop, compare the final current count with the maximum count and return the larger value
+        return maxCount>curCount? maxCount:curCount;
+        
+        // Approach Brute Force
+
+        // int count =0;
+        // for(int i=0;i<nums.length;i++){
+        //     int currEle = nums[i];
+        //     if(currEle != 1){
+        //         nums[i]= count;
+        //         count = 0;
+        //     }else{
+        //         count= count+1;
+        //     }
+        //     if(i == nums.length-1 ){
+        //         nums[i]=count;
+        //     }
+        // }
+        // int max = 0;
+        // for(int i =0; i<nums.length;i++){
+        //     if(nums[i]>max){
+        //         max = nums[i];
+        //     }
+        // }
+        // return max;
     }
 }
