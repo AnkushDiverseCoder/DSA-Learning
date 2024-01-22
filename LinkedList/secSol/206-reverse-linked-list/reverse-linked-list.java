2414d80ce6167c622f2578c392441a2196020c16
Thakur Ankush Singh <104247253+AnkushDiverseCoder@users.noreply.github.com>
2024-01-22 08:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (20.37%) - LeetSync

--- LinkedList/secSol/206-reverse-linked-list/reverse-linked-list.java
+++ LinkedList/secSol/206-reverse-linked-list/reverse-linked-list.java
@@ -10,16 +10,28 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        // Iterative
         // Three pointer algorithm 
         // prev curr next 
-            ListNode prev = null;
-            ListNode curr = head;
-            while(curr != null){
-                ListNode nxt = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = nxt;
-            }
-            return prev;
+            // ListNode prev = null;
+            // ListNode curr = head;
+            // while(curr != null){
+            //     ListNode nxt = curr.next;
+            //     curr.next = prev;
+            //     prev = curr;
+            //     curr = nxt;
+            // }
+            // return prev;
+
+        // Recursive
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+
     }
 }
