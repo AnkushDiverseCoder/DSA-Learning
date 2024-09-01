2f836f5bc33ffa7aabbe5c2a4fbfe70a8ecb256b
Thakur Ankush Singh <104247253+AnkushDiverseCoder@users.noreply.github.com>
2024-09-01 20:41:24 +0530

    Time: 6 ms (42.33%) | Memory: 43.2 MB (68.56%) - LeetSync

--- LinkedList/secSol/242-valid-anagram/valid-anagram.java
+++ LinkedList/secSol/242-valid-anagram/valid-anagram.java
@@ -8,15 +8,8 @@ class Solution {
         int[] arr = new int[26];
 
         for(int i = 0; i<s.length();i++) {
-            
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-
-            int index1 = (c1+0)%97;
-            int index2 = (c2+0)%97;
-
-            arr[index1]+= 1;
-            arr[index2]-= 1;
+            arr[(s.charAt(i)+0)%97]+= 1;
+            arr[(t.charAt(i)+0)%97]-= 1;
         }
 
         for(int i:arr){
