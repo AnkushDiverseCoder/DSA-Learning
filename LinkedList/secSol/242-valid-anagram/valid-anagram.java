ea41241ef4293365ce8b3c220f67831f5fafa31d
Thakur Ankush Singh <104247253+AnkushDiverseCoder@users.noreply.github.com>
2024-09-01 20:51:33 +0530

    Time: 1 ms (99.85%) | Memory: 43.7 MB (62.54%) - LeetSync

--- LinkedList/secSol/242-valid-anagram/valid-anagram.java
+++ LinkedList/secSol/242-valid-anagram/valid-anagram.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
-        if(s.length() != t.length()){
-            return false;
-        }
 
         int[] arr = new int[26];
-
-        for(int i = 0; i<s.length();i++) {
-            arr[ s.charAt(i) - 'a' ]+= 1;
-            arr[ t.charAt(i)- 'a' ]-= 1;
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
         }
-
+        for (char c : t.toCharArray()) {
+            arr[c - 'a']--;
+        }
         for(int i:arr){
             if(i!=0){
                 return false;
