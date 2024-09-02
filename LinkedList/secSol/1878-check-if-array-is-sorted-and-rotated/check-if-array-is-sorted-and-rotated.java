9e0f64a195e2177d8ad0fed40118a21302e493d3
Thakur Ankush Singh <104247253+AnkushDiverseCoder@users.noreply.github.com>
2024-09-02 20:45:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (37.31%) - LeetSync

--- LinkedList/secSol/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ LinkedList/secSol/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,23 +3,13 @@ class Solution {
         if(nums.length == 1){
             return true;
         }
-         for(int i =0 ; i< nums.length;i++){
-            int index = i;
-            boolean flag = false;
-            for(int j =0 ; j<nums.length-1; j++){
-                if(nums[index] <= nums[(index+1)% nums.length]){
-                    index = (index+1) % nums.length;
-                    flag = true;
-                }else{
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag == true ){
-                return true;
-            }
+        int flag = 0;
+        for(int i =0 ; i< nums.length;i++){
+          if(nums[i] > nums[(i+1)%nums.length]){
+            flag+=1;
+          }
         }
 
-        return false;
+        return flag <2;
     }
 }
