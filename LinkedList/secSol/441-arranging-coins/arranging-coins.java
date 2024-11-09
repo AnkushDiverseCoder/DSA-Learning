063114d00271dcdfa363d1b8ef47865d2e06cfd1
Thakur Ankush Singh <104247253+AnkushDiverseCoder@users.noreply.github.com>
2024-11-09 19:39:22 +0530

    Time: 1 ms (99.25%) | Memory: 41.5 MB (7.23%) - LeetSync

--- LinkedList/secSol/441-arranging-coins/arranging-coins.java
+++ LinkedList/secSol/441-arranging-coins/arranging-coins.java
@@ -1,14 +1,35 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int row =1 ;
-        int ele = n;
-        while (ele >=0){
-            if(ele < row){ 
-                return row-1;
+        // int row =1 ;
+        // int ele = n;
+        // while (ele >=0){
+        //     if(ele < row){ 
+        //         return row-1;
+        //     }
+        //     ele = ele - row; 
+        //     row = row +1; 
+        // }
+        // return -1;
+         
+        long l=1;
+        long r=n;
+        long mid=0;
+        long ans=0;
+
+        
+        while(l<=r){
+            mid=l+(r-l)/2;
+            
+            long no_of_coins=mid*(mid+1)/2;
+            
+            if(no_of_coins<=n){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
             }
-            ele = ele - row; 
-            row = row +1; 
         }
-        return -1;
+        return (int)ans;
+
     }
 }
